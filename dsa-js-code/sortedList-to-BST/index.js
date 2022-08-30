e145bad1ff87180a964e3809b0a27da18feb2300
Tarun kumar chourasiya <tkcthousand@gmail.com>
2022-08-30 09:55:19 +0530

    solve

---
+++ dsa-js-code/sortedList-to-BST/index.js
@@ -0,0 +1,55 @@
+class ListNode {
+    constructor(val, next) {
+        this.val = val;
+        this.next = next;
+    }
+}
+class TreeNode {
+    constructor(val,left,right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+let head = new ListNode(0);
+head.next = new ListNode(1);
+head.next.next = new ListNode(2);
+head.next.next.next = new ListNode(3);
+head.next.next.next.next = new ListNode(4);
+head.next.next.next.next.next = new ListNode(5);
+
+const printTree = (root) => {
+    if(root === null) return;
+    printTree(root.left);
+    console.log(root.val);
+    printTree(root.right);
+}
+
+var sortedListToBST = function(head) {
+	if(head === null){
+		return null;
+	}
+	let temp = new ListNode(-1,null);
+	temp.next = head;
+	const solve = (head) => {
+		if(head.next === null){ return null }
+		if(head.next.next === null){ return new ListNode(head.next.val,null,null)}
+		let fast = head;
+		let curr = head;
+		let prev = null;
+		while(fast !== null && fast.next !== null) {
+			fast = fast.next.next;
+			prev = curr;
+			curr = curr.next;
+		}
+		let ans = new TreeNode(curr.val,null,null);
+		prev.next = null;
+		ans.left = solve(head);
+		ans.right = solve(curr);
+		return ans;
+	}
+	return solve(temp);
+};
+
+let ans = sortedListToBST(head);
+printTree(ans);
