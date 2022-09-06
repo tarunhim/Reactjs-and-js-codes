e802aab8a665ab57d0f1ebebeaa4c508e54a52d1
Tarun kumar chourasiya <tkcthousand@gmail.com>
2022-09-06 12:47:49 +0530

    solve

--- reactjscodes/src/index.js
+++ reactjscodes/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './allcodes/Ecommorce-code/App';
+import App from './allcodes/login-and-authentication/App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
